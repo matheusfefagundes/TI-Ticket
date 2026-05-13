d75b68bc94f648cf107b619df1adc31a5e7176e3
Matheus Ferreira Fagundes <matheusferreirafagundes23@gmail.com>
2026-05-13 20:25:14 -0300

    fix: ajuste de segurança nos logs do prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -15,7 +15,7 @@ export const prisma =
     globalForPrisma.prisma || 
     new PrismaClient({
         adapter,
-        log: ["query"]
+        log: process.env.NODE_ENV === "development" ? ["warn", "error"] : ["error"]
     })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
